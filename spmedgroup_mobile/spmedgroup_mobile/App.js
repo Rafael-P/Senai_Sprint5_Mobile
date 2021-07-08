861089993f1580a790f493efbe73a83e57ca68b6
Rafael-P <r4f4el_porto@hotmail.com>
2021-07-07 21:18:13 -0300

    adc

--- spmedgroup_mobile/spmedgroup_mobile/App.js
+++ spmedgroup_mobile/spmedgroup_mobile/App.js
@@ -1,21 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
+import api from './src/services/api';
+
+export default class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      listaConsultas : []
+    };
+  }
+
+  buscarConsultas = async () => {
+    const resposta = await api.get ('/Consultas');
+
+    const dadosDaApi = resposta.data;
+    this.setState({ listaConsultas : dadosDaApi });
+  };
+
+  componentDidMount(){
+    this.buscarConsultas();
+  }
+
+  render () {
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{'Consultas'.toUpperCase()}</Text>
+
+    <View>
+      <FLatList
+      contentContainerStyle={styles.mainBodyConteudo}
+      data={this.state.listaConsultas}
+      keyExtractor={this.renderizaItem}
+      />
+    </View>
     </View>
   );
-}
+  }
+
+  renderizaItem = ({item}) => (
+    <View style={styles.flatItemLinha}>
+      <View>
+        <Text>{item.idConsulta}</Text>
+        <Text>{item.idPaciente}</Text>
+        <Text>{item.idMedico}</Text>
+        <Text>{item.idStatusConsulta}</Text>
+        <Text>{item.DataConsulta}</Text>
+        <Text>{item.HorarioConsulta}</Text>
+        <Text>{item.DescricaoAtendimento}</Text>
+      </View>
+    </View>
+  );
+
+}//fim
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  //conteudo da lista 
+
+  mainBodyConteudo : {
+    paddingTop : 30,
+    paddingRight : 50,
+    paddingLeft : 50
+  },
+
+  flatItemLinha : {
+    flexDirection : 'row',
+    borderBottomWidth : 0.9,
+    borderBottomColor : 'blue'
+  }
+
 });
