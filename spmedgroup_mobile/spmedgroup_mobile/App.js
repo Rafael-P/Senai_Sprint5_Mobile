b1c42d6b2e1d55299190e9b16682524da4a7ef69
Rafael Santos Porto <50736943846@info.local>
2021-07-08 10:47:16 -0300

    adc

--- spmedgroup_mobile/spmedgroup_mobile/App.js
+++ spmedgroup_mobile/spmedgroup_mobile/App.js
@@ -1,78 +1,21 @@
-import React, { Component } from 'react';
-import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import api from './src/services/api';
+import consultas from './src/screens/consultas';
+import login from './src/screens/login';
+import perfil from './src/screens/perfil';
 
-export default class App extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      listaConsultas : []
-    };
-  }
+const AuthStack = createStackNavigator();
 
-  buscarConsultas = async () => {
-    const resposta = await api.get ('/Consultas');
-
-    const dadosDaApi = resposta.data;
-    this.setState({ listaConsultas : dadosDaApi });
-  };
-
-  componentDidMount(){
-    this.buscarConsultas();
-  }
-
-  render () {
+export default function Stack(){
   return(
-    <View style={styles.container}>
-      <Text>{'Consultas'.toUpperCase()}</Text>
-
-    <View>
-      <FLatList
-      contentContainerStyle={styles.mainBodyConteudo}
-      data={this.state.listaConsultas}
-      keyExtractor={this.renderizaItem}
-      />
-    </View>
-    </View>
-  );
-  }
-
-  renderizaItem = ({item}) => (
-    <View style={styles.flatItemLinha}>
-      <View>
-        <Text>{item.idConsulta}</Text>
-        <Text>{item.idPaciente}</Text>
-        <Text>{item.idMedico}</Text>
-        <Text>{item.idStatusConsulta}</Text>
-        <Text>{item.DataConsulta}</Text>
-        <Text>{item.HorarioConsulta}</Text>
-        <Text>{item.DescricaoAtendimento}</Text>
-      </View>
-    </View>
-  );
-
-}//fim
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  //conteudo da lista 
-
-  mainBodyConteudo : {
-    paddingTop : 30,
-    paddingRight : 50,
-    paddingLeft : 50
-  },
-
-  flatItemLinha : {
-    flexDirection : 'row',
-    borderBottomWidth : 0.9,
-    borderBottomColor : 'blue'
-  }
-
-});
+    <NavigationContainer>
+        <AuthStack.Navigator headerMode = 'none'>
+          <AuthStack.Screen name ='consultas' component={consultas} />
+          <AuthStack.Screen name ='login' component={login} />
+          <AuthStack.Screen name ='perfil' component={perfil} />
+        </AuthStack.Navigator>
+    </NavigationContainer>
+  )
+}
